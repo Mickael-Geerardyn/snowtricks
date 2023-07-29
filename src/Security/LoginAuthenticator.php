7f02e8e19ee91d7daa7b24ca3f0ab9355e592757
Mickael-Geerardyn <root@DESKTOP-D1ECK70.localdomain>
2023-07-28 20:02:00 -0400

    symfony insight

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -59,10 +59,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 			return new RedirectResponse($this->urlGenerator->generate('home'));
 		}
 
-        // For example:
         return new RedirectResponse($this->urlGenerator->generate('home'));
-
-        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
