52da3ad0241c05a78457f6dfea0cd99ee0993f83
Mickael-Geerardyn <root@DESKTOP-D1ECK70.localdomain>
2023-08-03 21:07:20 -0400

    latest work

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -35,6 +35,19 @@ class AppFixtures extends Fixture
 	private SluggerService $sluggerService;
 
 	private UserPasswordHasherInterface $userPasswordHasher;
+	private array $tricks = ["La manière de rider" => "Tout d'abord, il faut savoir qu'il y a deux positions sur sa planche: regular ou goofy. Un rider regular aura son pied gauche devant et un rider goofy aura son pied droit devant. Après un certain moment, les planchistes sont capables de descendre dans les deux positions. Un rider regular qui descend en position goofy, dira qu'il descend « switch ». Généralement, une manœuvre sera considéré comme plus difficile quand elle est effectué « switch ».",
+		"Les grabs" => "Un grab consiste à attraper la planche avec la main pendant le saut. Le verbe anglais to grab signifie « attraper. » Il existe plusieurs types de grabs selon la position de la saisie et la main choisie pour l'effectuer,",
+		"Les rotations" => "On désigne par le mot « rotation » uniquement des rotations horizontales ; les rotations verticales sont des flips. Le principe est d'effectuer une rotation horizontale pendant le saut, puis d'atterrir en position switch ou normal. La nomenclature se base sur le nombre de degrés de rotation effectués",
+		"Les flips" => "Un flip est une rotation verticale. On distingue les front flips, rotations en avant, et les back flips, rotations en arrière. Il est possible de faire plusieurs flips à la suite, et d'ajouter un grab à la rotation.",
+		"Les rotations désaxées" => "Une rotation désaxée est une rotation initialement horizontale mais lancée avec un mouvement des épaules particulier qui désaxe la rotation. Il existe différents types de rotations désaxées (corkscrew ou cork, rodeo, misty, etc.) en fonction de la manière dont est lancé le buste. Certaines de ces rotations, bien qu'initialement horizontales, font passer la tête en bas.",
+		"Les slides" => "Un slide consiste à glisser sur une barre de slide. Le slide se fait soit avec la planche dans l'axe de la barre, soit perpendiculaire, soit plus ou moins désaxé.",
+		"Les one foot tricks" => "Figures réalisée avec un pied décroché de la fixation, afin de tendre la jambe correspondante pour mettre en évidence le fait que le pied n'est pas fixé. Ce type de figure est extrêmement dangereuse pour les ligaments du genou en cas de mauvaise réception.",
+		"Old school" => "Le terme old school désigne un style de freestyle caractérisée par en ensemble de figure et une manière de réaliser des figures passée de mode, qui fait penser au freestyle des années 1980 - début 1990 (par opposition à new school)",
+		"Backside Air" => "On commence tout simplement avec LE trick. Les mauvaises langues prétendent qu’un backside air suffit à reconnaître ceux qui savent snowboarder. Si c’est vrai, alors Nicolas Müller est le meilleur snowboardeur du monde. Personne ne sait s’étirer aussi joliment, ne demeure aussi zen, n’est aussi provocant dans la jouissance.",
+		"Switch Backside Rodeo 720" => "Si l’univers du snowboard a jamais disposé d’un scientifique, alors c’était David Benedek. Personne mieux que lui n’a su comment monter un kicker pour qu’un trick marche bien. En musique, on qualifie cela d’expérimental. Ce n’est pas un hasard si c’est précisément lui qui a eu l’idée de faire un switch BS rodeo.",
+		"BS 540 Seatbelt" => "Hitsch aurait tout aussi bien pu faire de la danse classique mais il s’est décidé pour la neige. Peut-être tout simplement parce qu’en Engadine, les montagnes sont plus séduisantes que les gymnases. Quoi qu’il en soit, quiconque arrive à attraper aussi tranquillement l’arrière de la planche avec la main avant pendant un BS 5 dans un half-pipe sans s’ouvrir les lèvres sur le coping devrait occuper une chaire à Cambridge sur les prodiges de la coordination.",
+		"FS 720 Japan" => "Si, dans le monde du snowboard, il y avait aujourd’hui encore quelque chose de comparable au rock’n’roll, Ben Ferguson en serait le Jim Morrison, haut la main. Son riding est radical, sans compromis et beau à voir. Bien entendu, rien ne se fait à moins de 200 km/h, pas même les FS 7 Japan dans le pipe.",
+		"Skate Skills" => "Scott «MacGyver» Stevens n’aurait en fait pas besoin de forfait de remontée. Scott n’aurait même pas besoin d’aller à la montagne. Scott a juste à sortir de chez lui, respirer un bon coup et démarrer. Après trois jours de tournage, son rôle serait plus long et plus créatif que pour ceux qui ont dû passer 20 heures en avion, 10 heures en voiture, 5 heures en Ski-Doo et 2 heures en hélicoptère pour leur séquence."];
 
 	public function __construct(SluggerService $sluggerService, UserPasswordHasherInterface $userPasswordHasher)
 	{
@@ -88,8 +101,9 @@ class AppFixtures extends Fixture
 	{
 		$lengthUserArray = count($this->objectUsersArray) -1;
 		$lengthGroupeArray = count($this->objectsGroupeArray) -1;
+		$imageNumber = 0;
 
-		for ($i = 0; $i < 10; $i++)
+		foreach($this->tricks as $name => $description)
 		{
 
 			$selectedUserObject = $this->objectUsersArray[mt_rand(0, $lengthUserArray)];
@@ -97,16 +111,17 @@ class AppFixtures extends Fixture
 
 			$this->figure = new Figure();
 
-			$this->figure->setName("Nom de la figure numéro ${i}");
-			$this->figure->setDescription("Description de la figure numéro ${i}");
+			$this->figure->setName($name);
+			$this->figure->setDescription($description);
 			$this->figure->setCreatedAt();
 			$this->figure->setGroupe($selectedGroupeObject);
 			$this->figure->setUser($selectedUserObject);
 			$this->figure->setSlug($this->sluggerService->makeSlug($this->figure->getName()));
 
 			self::loadMessages($manager, $selectedUserObject);
-			self::loadImage($i, $manager, $selectedUserObject);
+			self::loadImage($imageNumber, $manager, $selectedUserObject);
 
+			$imageNumber++;
 			$manager->persist($this->figure);
 		}
 	}
@@ -127,14 +142,14 @@ class AppFixtures extends Fixture
 		}
 	}
 
-	public function loadImage(int $i, ObjectManager $manager, User $selectedUserObject): void
+	public function loadImage(int $imageNumber,ObjectManager $manager, User $selectedUserObject): void
 	{
-		$image = new Image();
-		$image->setPath("picture-${i}.jpg");
-		$image->setFigure($this->figure);
-		$image->setUser($selectedUserObject);
-		$image->setBanner(true);
+			$image = new Image();
+			$image->setPath("picture-${imageNumber}.jpg");
+			$image->setFigure($this->figure);
+			$image->setUser($selectedUserObject);
+			$image->setBanner(true);
 
-		$manager->persist($image);
+			$manager->persist($image);
 	}
 }
